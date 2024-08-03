7fa3a88e4b9848a73fd30dee4b9f78a6be438217
Adarshayadav <adarshayadav696@gamil.com>
2024-08-03 17:51:10 +0530

    done

---
+++ src/test/java/com/adarsha/movie/ticket/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarsha.movie.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
